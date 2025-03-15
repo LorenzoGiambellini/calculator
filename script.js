eb87ee226ad9eb4cd677c5c7c50d0722d15ed8ad
Lorenzo Giambellini <lorenzo.giambellini@icloud.com>
2025-03-15 17:48:02 +0100

    add logic

--- script.js
+++ script.js
@@ -17,19 +17,16 @@ function divide(a, b) {
 function operate(a, b, operator){
     switch (operator) {
         case "+":
-            add(a, b);
-            break;
+            return add(a, b);
         case "-":
-            subtract(a, b);
-            break;
+            return subtract(a, b);
         case "*":
-            multiply(a, b);
-            break;
+            return multiply(a, b);
         case "/":
-            divide(a, b);
-            break;
+            return divide(a, b);
         default:
             console.log("ERROR");
+            return null;
     }
 }
 
@@ -47,9 +44,95 @@ const classMap = {
     "AC": "clear"
 };
 symbols.map(item => {
-    let btn = document.createElement("button");
-    let className = 'btn-' + (classMap[item] || item);    
+    const btn = document.createElement("button");
+    const className = 'btn-' + (classMap[item] || item);    
     btn.className = className;
     btn.textContent = item;
     container.appendChild(btn);
 });
+
+
+let a = '';
+let b = '';
+let operator = '';
+let minus = false; // positive by default
+const button = document.querySelectorAll("button");
+button.forEach(button => {
+    button.addEventListener("click", () => {
+    
+        switch(button.className) {
+            case "btn-0":
+                a += '0';
+                break;
+            case "btn-1":
+                a += '1';
+                break;
+            case "btn-2":
+                a += '2';
+                break;
+            case "btn-3":
+                a += '3';
+                break;
+            case "btn-4":
+                a += '4';
+                break;
+            case "btn-5":
+                a += '5';
+                break;
+            case "btn-6":
+                a += '6';
+                break;
+            case "btn-7":
+                a += '7';
+                break;
+            case "btn-8":
+                a += '8';
+                break;
+            case "btn-9":
+                a += '9';
+                break;
+            case "btn-dot":
+                a += '.';
+                break;
+            case "btn-clear":
+                a = '';
+                b = '';
+                break;
+            case "btn-add":
+                operator = '+';
+                b = a;
+                a = '';
+                break;
+            case "btn-subtract":
+                operator = '-';
+                b = a;
+                a = '';
+                break;
+            case "btn-multiply":
+                operator = '*';
+                b = a;
+                a = '';
+                break;
+            case "btn-divide":
+                operator = '/';
+                b = a;
+                a = '';
+                break;
+            case "btn-equals":
+                result = operate(Number(b), Number(a), operator);
+                console.log(result);
+                a = result.toString();
+                break;
+            case "btn-plus-minus":
+                if(minus == false) {
+                    a = '-' + a;
+                    minus = true;
+                }
+                else {
+                    a = a.slice(1);
+                    minus = false;
+                }
+                console.log(a);
+        }
+    });   
+})
