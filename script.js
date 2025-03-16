0673d25dec3a05e09b0ad5c8f182bbefef211675
Lorenzo Giambellini <lorenzo.giambellini@icloud.com>
2025-03-16 11:04:39 +0100

    update logic js

--- script.js
+++ script.js
@@ -56,75 +56,43 @@ let a = '';
 let b = '';
 let operator = '';
 const button = document.querySelectorAll("button");
+
 button.forEach(button => {
     button.addEventListener("click", () => {
-    
         switch(button.className) {
-            case "btn-0":
-                a += '0';
-                break;
-            case "btn-1":
-                a += '1';
-                break;
-            case "btn-2":
-                a += '2';
-                break;
-            case "btn-3":
-                a += '3';
-                break;
-            case "btn-4":
-                a += '4';
-                break;
-            case "btn-5":
-                a += '5';
-                break;
-            case "btn-6":
-                a += '6';
-                break;
-            case "btn-7":
-                a += '7';
-                break;
-            case "btn-8":
-                a += '8';
-                break;
-            case "btn-9":
-                a += '9';
-                break;
+            default:
+                if (!isNaN(button.textContent)) {
+                    a += button.textContent;
+                }                    break;
             case "btn-dot":
-                a += '.';
+                if(!a.includes('.')) a += '.';
                 break;
             case "btn-clear":
                 a = '';
                 b = '';
                 break;
+            case "btn-percent":
+                a = (Number(a)/(100)).toString();
+                break;
             case "btn-add":
-                operator = '+';
-                b = a;
-                a = '';
-                break;
             case "btn-subtract":
-                operator = '-';
-                b = a;
-                a = '';
-                break;
             case "btn-multiply":
-                operator = '*';
-                b = a;
-                a = '';
-                break;
             case "btn-divide":
-                operator = '/';
+                operator = button.textContent;
                 b = a;
                 a = '';
                 break;
             case "btn-equals":
-                result = operate(Number(b), Number(a), operator);
-                display.textContent = result;
-                console.log(result);
-                a = result.toString();
+                a = operate(Number(b), Number(a), operator);
+                a = a.toString();
                 break;
             case "btn-plus-minus":
                 a = (Number(a)*(-1)).toString();
+        }
+        if(a === ''){
+            display.textContent = Number(b);
+        } else {
+            display.textContent = Number(a);
         }
     });   
 })
